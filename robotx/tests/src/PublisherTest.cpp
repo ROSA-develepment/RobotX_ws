1e3284d804bd44bc0e6f019d26628d28e2cc396b
Jmendes <jmendes.3595@gmail.com>
2024-03-29 12:09:26 +0000

    RobotX-3 Create tests for Publisher

--- robotx/tests/src/PublisherTest.cpp
+++ robotx/tests/src/PublisherTest.cpp
@@ -1,18 +1,10 @@
 
-#include "PublisherTest.h"
-#include "rclcpp/rclcpp.hpp"
-//#include "rosa/publisher/Publisher.h"
+#include "rosa/publisher/Publisher.h"
 
 #include "gtest/gtest.h"
 
 
-TEST(PublisherTest, publishmessage)
+TEST(PublisherTestSuit, publishmessage)
 {
     ASSERT_EQ(1, 1);
-}
-
-int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
